db055e890e69011061bb21e0d432efb6e6d5c1f0
hmbool <1298541373@qq.com>
2020-01-16 14:42:33 +0800

    write new a file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
+    <leftNav></leftNav>
+    <div class="main">
+        <router-view/>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import leftNav from '@/components/common/leftNav'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    leftNav
+  }
 }
 </script>
 
@@ -17,6 +25,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+.main{
+  float: left;
+  width: 95%;
+  background-color: #EFF2F7;
+  height: 100%;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="pos">
+      <!-- Hello AwesomePos Demo -->
+      <el-row>
+        <el-col :span="7" class="pos-order" id="order-list">
+          <el-tabs>
+            <el-tab-pane label="点餐"> 
+              <el-table :data="tableData" border show-summary style="width:100%">
+                <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+                <el-table-column prop="count" label="数量" width="70"></el-table-column>
+                <el-table-column prop="price" label="金额" width="70%"></el-table-column>
+                <el-table-column label="操作" width="100%" fixed="right">
+                  <template scope="scope">
+                    <el-button type="text" size="small">删除</el-button>
+                    <el-button type="text" size="small">增加</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <div class="div-btn">
+                <el-button type="warning">挂单</el-button>
+                <el-button type="danger">删除</el-button>
+                <el-button type="success">结账</el-button>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="挂单"> 
+              
+            </el-tab-pane>
+            <el-tab-pane label="外卖"> 
+              
+            </el-tab-pane>
+          </el-tabs>
+        </el-col>
+        <el-col>
+          我是产品栏
+        </el-col>
+      </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos',
+  data () {
+    return {
+      tableData:[{
+
+          goodsName: '可口可乐',
+          price: 8,
+          count:1
+        }, {
+
+          goodsName: '香辣鸡腿堡',
+          price: 15,
+          count:1
+        }, {
+
+          goodsName: '爱心薯条',
+          price: 8,
+          count:1
+        }, {
+
+          goodsName: '甜筒',
+          price: 8,
+          count:1
+        }]
+    }
+  },
+  mounted () {
+    var orderHeight = document.body.clientHeight;
+    document.getElementById('order-list').style.height = orderHeight + 'px';
+  }
+}
+</script>
+
+
+<style scoped>
+.pos-order{
+  background-color: #F9FAFC;
+  border-left: 1px solid #C0CCDA;
+}
+
+.div-btn{
+  margin-top: 10px;
+}
+</style>
