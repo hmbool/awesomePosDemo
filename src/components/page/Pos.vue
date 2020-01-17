631086c52fa31e47a3a47c300c2ffa6888322f92
hmbool <1298541373@qq.com>
2020-01-17 14:56:10 +0800

    new file

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="pos">
-      <!-- Hello AwesomePos Demo -->
+    <div>
       <el-row>
         <el-col :span="7" class="pos-order" id="order-list">
           <el-tabs>
-            <el-tab-pane label="点餐"> 
+            <el-tab-pane label="点餐">
               <el-table :data="tableData" border show-summary style="width:100%">
                 <el-table-column prop="goodsName" label="商品名称"></el-table-column>
                 <el-table-column prop="count" label="数量" width="70"></el-table-column>
                 <el-table-column prop="price" label="金额" width="70%"></el-table-column>
                 <el-table-column label="操作" width="100%" fixed="right">
-                  <template scope="scope">
+                  <template slot-scope="scope">
+                    <!--.scope这个属性在最新版本vue已经被弃用，升级成slot-scope了 ，所以属性名应该改为slot-scope-->
                     <el-button type="text" size="small">删除</el-button>
                     <el-button type="text" size="small">增加</el-button>
                   </template>
@@ -22,64 +23,178 @@
                 <el-button type="success">结账</el-button>
               </div>
             </el-tab-pane>
-            <el-tab-pane label="挂单"> 
-              
-            </el-tab-pane>
-            <el-tab-pane label="外卖"> 
-              
-            </el-tab-pane>
+            <el-tab-pane label="挂单"></el-tab-pane>
+            <el-tab-pane label="外卖"></el-tab-pane>
           </el-tabs>
         </el-col>
-        <el-col>
-          我是产品栏
+        <!--商品展示-->
+        <el-col :span="17">
+          <div class="often-goods">
+            <div class="title">常用商品</div>
+            <div class="often-goods-list">
+              <ul>
+                <li v-for="goods in oftenGoods">
+                  <span>{{goods.goodsName}}</span>
+                  <span class="o-price">￥{{goods.price}}元</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="goods-type">
+            <el-tabs>
+              <el-tab-pane label="汉堡">
+                <div>
+                  <span class="cookList">
+                    <li v-for="goods in type0Goods">
+                      <span class="foodImg">
+                        <img :src="goods.goodsImg" />
+                      </span>
+                      <span class="foodName">{{goods.goodsName}}</span>
+                      <span class="foodPrice">￥{{goods.price}}元</span>
+                    </li>
+                  </span>
+                </div>
+              </el-tab-pane>
+              <el-tab-pane label="小食">小食</el-tab-pane>
+              <el-tab-pane label="饮料">饮料</el-tab-pane>
+              <el-tab-pane label="套餐">套餐</el-tab-pane>
+            </el-tabs>
+          </div>
         </el-col>
       </el-row>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'pos',
-  data () {
+  name: "Pos",
+  data() {
     return {
-      tableData:[{
-
-          goodsName: '可口可乐',
+      tableData: [
+        {
+          goodsName: "可口可乐",
           price: 8,
-          count:1
-        }, {
-
-          goodsName: '香辣鸡腿堡',
+          count: 1
+        },
+        {
+          goodsName: "香辣鸡腿堡",
           price: 15,
-          count:1
-        }, {
-
-          goodsName: '爱心薯条',
+          count: 1
+        },
+        {
+          goodsName: "爱心薯条",
           price: 8,
-          count:1
-        }, {
-
-          goodsName: '甜筒',
+          count: 1
+        },
+        {
+          goodsName: "甜筒",
           price: 8,
-          count:1
-        }]
-    }
+          count: 1
+        }
+      ],
+      oftenGoods: [],
+      type0Goods: [],
+      type1Goods: [],
+      type2Goods: [],
+      type3Goods: [],
+    };
   },
-  mounted () {
+  
+  created () {
+    axios.get('https://www.easy-mock.com/mock/5e1abb7d7f109b0caa4d2e32/vue-pos/oftenGoods')
+    axios.get('https://www.easy-mock.com/mock/5e1abb7d7f109b0caa4d2e32/vue-pos/typeGoods')
+    .then(reponse=>{
+      console.log(response);
+      this.oftenGoods = response.data.oftenGoods;
+      this.type0Goods = response.data.data[0];
+      this.type1Goods = response.data.data[1];
+      this.type2Goods = response.data.data[2];
+      this.type3Goods = response.data.data[3];
+    })
+    .catch(error=>{
+      console.log(error);
+      alert('网络错误，不能访问');
+    })
+    
+  },
+  mounted() {
     var orderHeight = document.body.clientHeight;
-    document.getElementById('order-list').style.height = orderHeight + 'px';
+    document.getElementById("order-list").style.height = orderHeight + "px";
   }
-}
+};
 </script>
-
-
 <style scoped>
-.pos-order{
-  background-color: #F9FAFC;
-  border-left: 1px solid #C0CCDA;
+.pos-order {
+  background-color: #f9fafc;
+  border-left: 1px solid #c0ccda;
 }
 
-.div-btn{
+.div-btn {
   margin-top: 10px;
+}
+
+.title {
+  height: 20px;
+  border-bottom: 1px solid #d3dce6;
+  background-color: #f9fafc;
+  padding: 10px;
+  text-align: left;
+}
+.often-goods-list ul li {
+  list-style: none;
+  float: left;
+  border: 1px solid #e5e9f2;
+  padding: 10px;
+  margin: 10px;
+  background-color: #fff;
+}
+.o-price {
+  color: #58b7ff;
+}
+.goods-type {
+  clear: both;
+}
+.cookList li {
+  list-style: none;
+  width: 23%;
+  border: 1px solid #e5e9f2;
+  height: auot;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 2px;
+  float: left;
+  margin: 2px;
+}
+.cookList li {
+  list-style: none;
+  width: 23%;
+  border: 1px solid #e5e9f2;
+  height: auot;
+  overflow: hidden;
+  background-color: #fff;
+  padding: 2px;
+  float: left;
+  margin: 2px;
+}
+.cookList li span {
+  display: block;
+  float: left;
+}
+.foodImg {
+  width: 40%;
+}
+.foodName {
+  font-size: 18px;
+  padding-left: 10px;
+  color: brown;
+}
+.foodPrice {
+  font-size: 16px;
+  padding-left: 10px;
+  padding-top: 10px;
 }
 </style>
